afb03ba49fc611c3a4f4b0a99e014f902e3b962f
1anch0 <110996577+1anch0@users.noreply.github.com>
2022-08-11 16:44:57 -0400

    Add files via upload

---
+++ Mini_Project_2/ViewController3.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController3.swift
+//  Mini_Project_2
+//
+//  Created by Scholar on 8/11/22.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
